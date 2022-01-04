4763a7ce9124bcd23907caf9adcee9292510cdf0
Sergii Penner <sergmister1@gmail.com>
2022-01-04 11:15:18 -0700

    Add About section

---
+++ src/routes/about/index.svelte
@@ -0,0 +1,7 @@
+<h1>About Sergii Penner</h1>
+
+<p>
+	I am a software developer and AI enthusiast studying Applied Mathematics and Computer Science at the University of
+	Alberta. I enjoy learning, and have learned for too many programming languages and frameworks for my own good. I also
+	like doing activies such as hiking, climbing, running, biking, and swimming.
+</p>
